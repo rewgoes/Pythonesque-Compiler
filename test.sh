e177f46ce4227304d3e1bfad25988c68f928a356
tnromero <tnromero@gmail.com>
2013-10-27 15:23:21 -0200

    LEIAME.txt

--- test.sh
+++ test.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-INPUT=Arquivos_teste/arquivos_com_1_erro/entrada/*
+INPUT1=Arquivos_teste/arquivos_com_1_erro/entrada/*
 
 # Remove previous folder
 rm -rf testoutput/
 
 # Create folder
 mkdir testoutput/
+mkdir testoutput/arquivos_com_1_erro/
 
-for f in $INPUT
+for f in $INPUT1
 do
     # Extract filename from path
     fname=`basename $f`
 
     # Execute pythonesque on test case
-    python pythonesque.py $f testoutput/tested-$fname
+    python pythonesque.py $f testoutput/arquivos_com_1_erro/tested-$fname
 
     # Compare output with sample
-    out=`diff testoutput/tested-$fname Arquivos_teste/arquivos_com_1_erro/saida/$fname`
+    out=`diff testoutput/arquivos_com_1_erro/tested-$fname Arquivos_teste/arquivos_com_1_erro/saida/$fname`
 
     if [ -n "$out" ] 
     then
@@ -28,3 +29,27 @@ do
     fi
 done
 
+INPUT2=Arquivos_teste/arquivos_sem_erros/entrada/*
+
+# Create folder
+mkdir testoutput/arquivos_sem_erros/
+
+for f in $INPUT2
+do
+    # Extract filename from path
+    fname=`basename $f`
+
+    # Execute pythonesque on test case
+    python pythonesque.py $f testoutput/arquivos_sem_erros/tested-$fname
+
+    # Compare output with sample
+    out=`diff testoutput/arquivos_sem_erros/tested-$fname Arquivos_teste/arquivos_sem_erros/saida/$fname`
+
+    if [ -n "$out" ] 
+    then
+        # Error
+        echo "Error on $fname..."
+    else
+        echo "$fname OK"
+    fi
+done
---
+++ test_com_1_erro.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+INPUT=Arquivos_teste/arquivos_com_1_erro/entrada/*
+
+# Remove previous folder
+rm -rf testoutput/
+
+# Create folder
+mkdir testoutput/
+
+for f in $INPUT
+do
+    # Extract filename from path
+    fname=`basename $f`
+
+    # Execute pythonesque on test case
+    python pythonesque.py $f testoutput/tested-$fname
+
+    # Compare output with sample
+    out=`diff testoutput/tested-$fname Arquivos_teste/arquivos_com_1_erro/saida/$fname`
+
+    if [ -n "$out" ] 
+    then
+        # Error
+        echo "Error on $fname..."
+    else
+        echo "$fname OK"
+    fi
+done
+
